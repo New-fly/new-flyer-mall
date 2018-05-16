0b30a75c0d8e8abdf53f0d864d5c6870c0490d8d
FSJAlger <fsj.wlgzs@gmail.com>
2018-05-16 14:41:00 +0800

    pull

--- src/main/java/org/wlgzs/xf_mall/controller/admin/ProductController.java
+++ src/main/java/org/wlgzs/xf_mall/controller/admin/ProductController.java
@@ -37,7 +37,7 @@ public class ProductController {
      */
     @RequestMapping(value = "/adminProductList")
     public ModelAndView list(Model model, @RequestParam(value = "page",defaultValue = "0") int page,
-                             @RequestParam(value = "limit",defaultValue = "10") int limit) {
+                             @RequestParam(value = "limit",defaultValue = "8") int limit) {
         String product_keywords ="";
         if(page != 0) page--;
         Page pages =  productService.getProductListPage(product_keywords,page,limit);
@@ -64,7 +64,7 @@ public class ProductController {
      */
     @RequestMapping("/adminFindProduct")
     public  ModelAndView findProduct(Model model, String product_keywords, @RequestParam(value = "page",defaultValue = "0") int page,
-                                     @RequestParam(value = "limit",defaultValue = "10") int limit){
+                                     @RequestParam(value = "limit",defaultValue = "8") int limit){
         if(page != 0) page--;
         Page pages =  productService.getProductListPage(product_keywords,page,limit);
         model.addAttribute("TotalPages", pages.getTotalPages());//查询的页数
@@ -161,7 +161,7 @@ public class ProductController {
      */
     @RequestMapping("/productCategoryList")
     public ModelAndView category(Model model, @RequestParam(value = "page",defaultValue = "0") int page,
-                                 @RequestParam(value = "limit",defaultValue = "10") int limit){
+                                 @RequestParam(value = "limit",defaultValue = "8") int limit){
         String category_name ="";
         if(page != 0) page--;
         Page pages =  productService.getProductCategoryList(category_name,page,limit);
