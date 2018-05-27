4d50098b18191a8a7cf56e348bad52bdc1ab0153
HuYaXing <911090257@qq.com>
2018-05-27 08:39:35 +0800

    push

--- src/main/java/org/wlgzs/xf_mall/dao/ProductRepository.java
+++ src/main/java/org/wlgzs/xf_mall/dao/ProductRepository.java
@@ -40,7 +40,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>,JpaSpeci
     @Query("SELECT o FROM Product o ")
     List<Product> getProductList(Sort sort);
 
-<<<<<<< HEAD
     @Query("SELECT o FROM Product o WHERE o.product_mallPrice BETWEEN ?1 AND ?2")
     List<Product> findByPrice(float minPrice,float maxPrice);
 
@@ -49,9 +48,8 @@ public interface ProductRepository extends JpaRepository<Product, Long>,JpaSpeci
 
     @Query("SELECT o FROM Product o WHERE o.product_mallPrice > ?1")
     List<Product> findByMaxPrice(float maxPrice);
-=======
+
     @Query("SELECT o FROM Product o WHERE o.product_keywords like %:product_keywords%")
     List<Product> findProductByProductKeywords(@Param(value = "product_keywords") String product_keywords);
 
->>>>>>> 9ef55565f75c32a1d518047f2702265b58705908
 }
