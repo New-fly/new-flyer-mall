aec2561d8076691ce92e6e93fd308c974707a98d
wangjingxiao <2962784374@qq.com>
2018-06-01 10:55:49 +0800

    pull

--- src/main/resources/static/js/addProductOneCategory.js
+++ src/main/resources/static/js/addProductOneCategory.js
@@ -1,18 +1,18 @@
-window.onload=function () {
-    var category_name = document.getElementById('category_name');
-    var file = document.getElementById('file');
-    var add = document.getElementById('add');
-    add.onclick = function () {
-        if (category_name.value == null || category_name.value == "") {
-            alert("请写入分类名称");
-            return false;
-        }
-        if (file.value == null || file.value == "") {
-            alert("请上传文件");
-            return false;
-        } else {
-            return true;
-        }
-
-    };
-};
+// window.onload=function () {
+//     var category_name = document.getElementById('category_name');
+//     var file = document.getElementById('file');
+//     var add = document.getElementById('add');
+//     add.onclick = function () {
+//         if (category_name.value == null || category_name.value == "") {
+//             alert("请写入分类名称");
+//             return false;
+//         }
+//         if (file.value == null || file.value == "") {
+//             alert("请上传文件");
+//             return false;
+//         } else {
+//             return true;
+//         }
+//
+//     };
+// };
